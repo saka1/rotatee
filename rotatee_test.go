00a88f7640c62f01358d0fd7831815c9d2236bb9
saka1 <github@saka1.net>
2017-11-13 09:30:37 +0900

    add another test

--- rotatee_test.go
+++ rotatee_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -21,5 +22,38 @@ func Test_rotatee_simpleTeeTest(t *testing.T) {
 	rotatee.Start()
 	if str, err := stdout.ReadString('c'); str != "abc" || err != nil {
 		t.Fatalf("invalid stdout: %v, %v", str, err)
+	}
+}
+
+func Test_rotatee_sizeBasedRotation(t *testing.T) {
+	// Suppress logger
+	origLevel := log.Level
+	log.SetLevel(logrus.ErrorLevel)
+	defer log.SetLevel(origLevel)
+
+	var stdin, stdout bytes.Buffer
+	tmpdir, _ := ioutil.TempDir("", "")
+	defer os.RemoveAll(tmpdir)
+	rotatee := NewRotatee(RotateeSetting{
+		stdin:       &stdin,
+		stdout:      &stdout,
+		args:        []string{filepath.Join(tmpdir, "foo%i.log")},
+		maxFileSize: 3,
+		historySize: 2,
+	})
+	stdin.Write([]byte("abcdefghi"))
+	rotatee.Start()
+	if str, err := stdout.ReadString('f'); str != "abcdef" || err != nil {
+		t.Fatalf("invalid stdout: %v, %v", str, err)
+	}
+	if b, err := ioutil.ReadFile(filepath.Join(tmpdir, "foo.log")); err != nil || string(b) != "ghi" {
+		t.Fatalf("invalid stdout: %v, %v", b, err)
+	}
+	if b, err := ioutil.ReadFile(filepath.Join(tmpdir, "foo1.log")); err != nil || string(b) != "def" {
+		t.Fatalf("invalid stdout: %v, %v", b, err)
+	}
+	_, err := os.Stat(filepath.Join(tmpdir, "foo2.log"))
+	if err == nil {
+		t.Fatalf("'foo2.log' must not exist")
 	}
 }
